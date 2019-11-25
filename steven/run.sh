012120d7ca05a798ce722419208fd1ac40f38b14
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-11-25 17:52:31 -0600

    Create run.sh

---
+++ steven/run.sh
@@ -0,0 +1,46 @@
+cd /gpfs/home/guosa/hpc/project/pmrp/
+mkdir cytokine
+cd cytokine
+# find the raw phase 2 PMRP plink dataset
+cd /gpfs/home/guosa/hpc/project/pmrp/phase1
+touch -m FinalRelease_QC_20140311_Team1_Marshfield.bim
+touch -m FinalRelease_QC_20140311_Team1_Marshfield.fam
+touch -m FinalRelease_QC_20140311_Team1_Marshfield.bed
+
+# find the raw phase 2 PMRP plink dataset
+cd /gpfs/home/guosa/hpc/project/pmrp/phase2
+cp S_Hebbring_Unr.Guo.Forward.bed FinalRelease_QC_20190311_Team1_Marshfield.bed
+cp S_Hebbring_Unr.Guo.Forward.bim FinalRelease_QC_20190311_Team1_Marshfield.bim
+cp S_Hebbring_Unr.Guo.Forward.fam FinalRelease_QC_20190311_Team1_Marshfield.fam
+
+# read phase 1 and phase 2 bim file
+bim1<-read.table("../phase1/FinalRelease_QC_20140311_Team1_Marshfield.bim")
+bim2<-read.table("../phase2/FinalRelease_QC_20190311_Team1_Marshfield.bim")
+
+head(bim1)
+head(bim2)
+
+bimid1<-paste(bim1[,1],bim1[,4],bim1[,5],bim1[,6],sep=":")
+bimid2<-paste(bim2[,1],bim2[,4],bim2[,5],bim2[,6],sep=":")
+
+bim<-na.omit(bim2[match(bimid1,bimid2),])
+out<-data.frame(bim[,1],bim[,4])
+write.table(out,file="sharedconservedSNP.bed",sep="\t",quote=F,col.names=F,row.names=F)
+
+cp ../phase1/FinalRelease_QC_20140311_Team1_Marshfield.bim ./
+cp ../phase1/FinalRelease_QC_20140311_Team1_Marshfield.fam ./
+cp ../phase1/FinalRelease_QC_20140311_Team1_Marshfield.bed ./
+
+cp ../phase2/FinalRelease_QC_20190311_Team1_Marshfield.bim ./
+cp ../phase2/FinalRelease_QC_20190311_Team1_Marshfield.fam ./
+cp ../phase2/FinalRelease_QC_20190311_Team1_Marshfield.bed ./
+
+plink2 --bfile FinalRelease_QC_20190311_Team1_Marshfield --recode vcf --out FinalRelease_QC_20190311_Team1_Marshfield
+plink2 --bfile FinalRelease_QC_20140311_Team1_Marshfield --recode vcf --out FinalRelease_QC_20140311_Team1_Marshfield
+
+bcftools view FinalRelease_QC_20140311_Team1_Marshfield.vcf -Oz -o FinalRelease_QC_20190311_Team1_Marshfield.vcf.gz
+bcftools view FinalRelease_QC_20140311_Team1_Marshfield.vcf -Oz -o FinalRelease_QC_20140311_Team1_Marshfield.vcf.gz
+
+tabix -p vcf FinalRelease_QC_20190311_Team1_Marshfield.vcf.gz
+tabix -p vcf FinalRelease_QC_20140311_Team1_Marshfield.vcf.gz
+
